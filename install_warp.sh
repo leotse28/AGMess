9d0cf681ff68e2023c5cf42f11a56e8afd0592eb
lyndon <xieliangkk@outlook.com>
2026-04-26 19:00:46 +0800

    Update install_warp.sh

--- install_warp.sh
+++ install_warp.sh
@@ -1,1 +1,2 @@
-uuid="dadf0ebf-034a-49cf-9f46-5a48ef01ae60" vmpt="8081" argo="vmpt" agn="bloombergnf.x5411y.cloudns.biz" agk="eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiZTcyNjljODctYWVkZC00OWY5LTliNDctZDIxNzFjOTQxOWUzIiwicyI6Ik5UWTJPR1l4TkdZdE9XSmpNaTAwWWpNMExUZzJZell0TmpKbE1ETXhNVFZsWlRGaCJ9" bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh)
+uuid="dadf0ebf-034a-49cf-9f46-5a48ef01ae60" vmpt="8081" argo="vmpt" agn="gl2.globalleo.dpdns.org" agk="eyJhIjoiYWExZDM1MDYzNGQ1MDA2OGJkMGZkZDIxNjA3MGZhZTUiLCJ0IjoiNDIwODFkNGItMzExZS00YWVkLTk0ZTYtZjE3NjQyODZjMjM4IiwicyI6IllUQTFOVGM1TVRNdFpXTTVaaTAwTUdabExXRm1OamN0T1RrME5UQmlOMlF6TkdZMSJ9" bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosbx/main/argosbx.sh)
+
